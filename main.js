903c81d2c9050920e0d289a7de353f6592a78387
devzeshan <zeshan.ahmed@programmer.net>
2023-12-04 06:31:05 -0500

    Additions made to automated program

--- main.js
+++ main.js
@@ -6,18 +6,22 @@ function getComputerChoice() {
     return (numberItem == 1) ? 'Rock' : (numberItem == 2) ? 'Paper' : 'Scizzors'
 };
 
+// return (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock') ? 'YOU WIN!' : (playerSelection == computerSelection) ? 'TIE! NEXT ROUND!\n' + playRound(playerSelection, getComputerChoice()) : 'YOU LOSE!';
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    if (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock' ) {
-        return 'YOU WIN!';
-    } else if (playerSelection == computerSelection) {
-        return 'TIE! NEXT ROUND!\n' + playRound(playerSelection, getComputerChoice());
-    } else {
-        return 'YOU LOSE!';
-    }
-}
+    return (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock') ? 'YOU WIN!' : (playerSelection == computerSelection) ? 'TIE! NEXT ROUND!\n' + playRound(playerSelection, getComputerChoice()) : 'YOU LOSE!';
+
+    // if (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock' ) {
+    //     return 'YOU WIN!';
+    // } else if (playerSelection == computerSelection) {
+    //     return 'TIE! NEXT ROUND!\n' + playRound(playerSelection, getComputerChoice());
+    // } else {
+    //     return 'YOU LOSE!';
+    // }
+};
    
 
 console.log(playRound(playerSelection, computerSelection));
