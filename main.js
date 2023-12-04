a75c617143fffa289ee44f621178fa8dc7c179b9
devzeshan <zeshan.ahmed@programmer.net>
2023-12-04 06:55:11 -0500

    Code in-progress

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
 function getComputerChoice() {
     var numberItem = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     return (numberItem == 1) ? 'Rock' : (numberItem == 2) ? 'Paper' : 'Scizzors'
 };
 
-// return (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock') ? 'YOU WIN!' : (playerSelection == computerSelection) ? 'TIE! NEXT ROUND!\n' + playRound(playerSelection, getComputerChoice()) : 'YOU LOSE!';
-
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-
-    return (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock') ? 'YOU WIN!' : (playerSelection == computerSelection) ? 'TIE! NEXT ROUND!\n' + playRound(playerSelection, getComputerChoice()) : 'YOU LOSE!';
-
-    // if (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock' ) {
-    //     return 'YOU WIN!';
-    // } else if (playerSelection == computerSelection) {
-    //     return 'TIE! NEXT ROUND!\n' + playRound(playerSelection, getComputerChoice());
-    // } else {
-    //     return 'YOU LOSE!';
-    // }
+    return (playerSelection == 'rock' && computerSelection == 'scizzors' || playerSelection == 'scizzor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock') ? 'YOU WIN!' : (playerSelection == computerSelection) ? 'TIE! NEXT ROUND!\n' + game() : 'YOU LOSE!';
 };
-   
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (var i = 0; i < 5; i++) {
+        var playerSelection = prompt("CHOOSE! ROCK, PAPER, OR SCIZZORS!\n");
+        return playRound(playerSelection, getComputerChoice());
+    }
+}
+
+game();
